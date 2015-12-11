690b051b465ec6d6da7eca9326b8f0f055e13a9b
KyriakosAlexandrou <kyriakos.a.projects@gmail.com>
2015-12-11 00:20:57 +0000

    getCompanyRequest now uses the EventBus
    getCompanyRequest now uses the EventBus for callbacks to the UI

--- app/src/main/java/com/kyriakosalexandrou/fuse_test_v11/MainActivity.java
+++ app/src/main/java/com/kyriakosalexandrou/fuse_test_v11/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kyriakosalexandrou.fuse_test_v11;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
@@ -7,8 +8,12 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.kyriakosalexandrou.fuse_test_v11.events.CompanyEvent;
+import com.kyriakosalexandrou.fuse_test_v11.events.ErrorEvent;
+import com.kyriakosalexandrou.fuse_test_v11.models.Company;
 import com.kyriakosalexandrou.fuse_test_v11.services.CompanyService;
 
+import de.greenrobot.event.EventBus;
 import retrofit.RestAdapter;
 
 
@@ -56,7 +61,32 @@ public class MainActivity extends AppCompatActivity implements IuiHelper {
     private void getCompany(String companyName) {
         if (companyName.length() > 0) {
             CompanyService mCompanyService = new CompanyService(mRestAdapter);
-            mCompanyService.getCompanyRequest(companyName);
+            mCompanyService.getCompanyRequest(companyName, new CompanyEvent());
         }
+    }
+
+    public void onEventMainThread(CompanyEvent event) {
+        EventBus.getDefault().removeStickyEvent(event);
+        Company company = event.getCompany();
+        mCompanyNameUserInput.setText(company.getName());
+        mCompanyNameUserInput.setBackgroundColor(Color.GREEN);
+    }
+
+    public void onEventMainThread(ErrorEvent event) {
+        EventBus.getDefault().removeStickyEvent(event);
+        Util.showToastMessageCentered(this, event.getErrorMessage());
+        mCompanyNameUserInput.setBackgroundColor(Color.RED);
+    }
+
+    @Override
+    protected void onStart() {
+        EventBus.getDefault().registerSticky(this);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        EventBus.getDefault().unregister(this);
+        super.onStop();
     }
 }
--- app/src/main/java/com/kyriakosalexandrou/fuse_test_v11/services/CompanyService.java
+++ app/src/main/java/com/kyriakosalexandrou/fuse_test_v11/services/CompanyService.java
@@ -1,9 +1,13 @@
 package com.kyriakosalexandrou.fuse_test_v11.services;
 
+import android.app.usage.UsageEvents;
 import android.util.Log;
 
+import com.kyriakosalexandrou.fuse_test_v11.events.CompanyEvent;
+import com.kyriakosalexandrou.fuse_test_v11.events.ErrorEvent;
 import com.kyriakosalexandrou.fuse_test_v11.models.Company;
 
+import de.greenrobot.event.EventBus;
 import retrofit.Callback;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
@@ -23,18 +27,21 @@ public class CompanyService {
         mService = restAdapter.create(ICompanyService.class);
     }
 
-    public void getCompanyRequest(final String companyName) {
+    public void getCompanyRequest(final String companyName, final CompanyEvent event) {
 
         mService.getCompany(companyName, new Callback<Company>() {
 
                     @Override
                     public void success(Company company, Response response) {
                         Log.v(TAG, "success");
+                        event.setCompany(company);
+                        EventBus.getDefault().postSticky(event);
                     }
 
                     @Override
                     public void failure(RetrofitError error) {
                         Log.v(TAG, "failure");
+                        EventBus.getDefault().postSticky(new ErrorEvent("Failed to receive company details"));
                     }
                 }
         );
